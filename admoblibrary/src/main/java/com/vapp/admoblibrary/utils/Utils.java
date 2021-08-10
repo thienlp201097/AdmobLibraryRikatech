8288802af96846cb44eca2adff5501cd30b97ccf
NAMDH <namdh1212@gmail.com>
2021-08-10 14:27:10 +0700

    no message

--- admoblibrary/src/main/java/com/vapp/admoblibrary/utils/Utils.java
+++ admoblibrary/src/main/java/com/vapp/admoblibrary/utils/Utils.java
@@ -1,5 +1,6 @@
 package com.vapp.admoblibrary.utils;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -16,6 +17,8 @@ import androidx.fragment.app.FragmentTransaction;
 import com.vapp.admoblibrary.R;
 import com.vapp.admoblibrary.ads.model.AdUnitListModel;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -38,7 +41,7 @@ public class Utils {
     String countryCode = "";
 
     public boolean checkCountries(Context context, AdUnitListModel adUnitList) {
-        countryCode = getCurrentCountry(context);
+        countryCode = getDeviceCountryCode(context);
         boolean isShowAds = false;
         if (adUnitList.getCountries() == null) {
             isShowAds = true;
@@ -74,6 +77,88 @@ public class Utils {
         }
         return countryCode;
     }
+
+    private static String getDeviceCountryCode(Context context) {
+        String countryCode;
+
+        // try to get country code from TelephonyManager service
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        if(tm != null) {
+            // query first getSimCountryIso()
+            countryCode = tm.getSimCountryIso();
+            if (countryCode != null && countryCode.length() == 2)
+                return countryCode.toLowerCase();
+
+            if (tm.getPhoneType() == TelephonyManager.PHONE_TYPE_CDMA) {
+                // special case for CDMA Devices
+                countryCode = getCDMACountryIso();
+            } else {
+                // for 3G devices (with SIM) query getNetworkCountryIso()
+                countryCode = tm.getNetworkCountryIso();
+            }
+
+            if (countryCode != null && countryCode.length() == 2)
+                return countryCode.toLowerCase();
+        }
+
+        // if network country not available (tablets maybe), get country code from Locale class
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            countryCode = context.getResources().getConfiguration().getLocales().get(0).getCountry();
+        } else {
+            countryCode = context.getResources().getConfiguration().locale.getCountry();
+        }
+
+        if (countryCode != null && countryCode.length() == 2)
+            return  countryCode.toLowerCase();
+        // general fallback to "us"
+        return "us";
+    }
+
+    @SuppressLint("PrivateApi")
+    private static String getCDMACountryIso() {
+        try {
+            // try to get country code from SystemProperties private class
+            Class<?> systemProperties = Class.forName("android.os.SystemProperties");
+            Method get = systemProperties.getMethod("get", String.class);
+
+            // get homeOperator that contain MCC + MNC
+            String homeOperator = ((String) get.invoke(systemProperties,
+                    "ro.cdma.home.operator.numeric"));
+
+            // first 3 chars (MCC) from homeOperator represents the country code
+            int mcc = Integer.parseInt(homeOperator.substring(0, 3));
+
+            // mapping just countries that actually use CDMA networks
+            switch (mcc) {
+                case 330: return "PR";
+                case 310: return "US";
+                case 311: return "US";
+                case 312: return "US";
+                case 316: return "US";
+                case 283: return "AM";
+                case 460: return "CN";
+                case 455: return "MO";
+                case 414: return "MM";
+                case 619: return "SL";
+                case 450: return "KR";
+                case 634: return "SD";
+                case 434: return "UZ";
+                case 232: return "AT";
+                case 204: return "NL";
+                case 262: return "DE";
+                case 247: return "LV";
+                case 255: return "UA";
+            }
+        } catch (ClassNotFoundException ignored) {
+        } catch (NoSuchMethodException ignored) {
+        } catch (IllegalAccessException ignored) {
+        } catch (InvocationTargetException ignored) {
+        } catch (NullPointerException ignored) {
+        }
+
+        return null;
+    }
+
 
 
     public void showMessenger(Context context, String content, int time) {
