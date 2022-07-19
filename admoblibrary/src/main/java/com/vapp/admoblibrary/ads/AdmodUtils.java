4bd3b41dfa34e548e7f6689ef81dddfd92b0f044
NAMDH <>
2022-07-19 15:28:56 +0700

    gone nativead

--- admoblibrary/src/main/java/com/vapp/admoblibrary/ads/AdmodUtils.java
+++ admoblibrary/src/main/java/com/vapp/admoblibrary/ads/AdmodUtils.java
@@ -292,7 +292,7 @@ public class AdmodUtils {
 
                         viewGroup.removeAllViews();
                         viewGroup.addView(adView);
-                        viewGroup.setVisibility(View.VISIBLE);
+                        //viewGroup.setVisibility(View.VISIBLE);
                     }
 
                 })
@@ -351,7 +351,7 @@ public class AdmodUtils {
                         shimmerFrameLayout.stopShimmerAnimation();
                         viewGroup.removeAllViews();
                         viewGroup.addView(adView);
-                        viewGroup.setVisibility(View.VISIBLE);
+                        //viewGroup.setVisibility(View.VISIBLE);
                     }
 
                 })
