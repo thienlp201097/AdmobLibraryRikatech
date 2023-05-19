460507acaba6b442c4fc27b797d64aafe2d95bb6
thienlp <thienlp@dktechgroup.com>
2023-05-19 14:17:48 +0700

    fixx onresume crash

--- admoblibrary/src/main/java/com/vapp/admoblibrary/ads/AppOpenManager.java
+++ admoblibrary/src/main/java/com/vapp/admoblibrary/ads/AppOpenManager.java
@@ -260,7 +260,12 @@ public class AppOpenManager implements Application.ActivityLifecycleCallbacks, L
     public void showAdIfAvailable(final boolean isSplash) {
         if (!ProcessLifecycleOwner.get().getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
             if (fullScreenContentCallback != null) {
-                dialogFullScreen.dismiss();
+                try {
+                    dialogFullScreen.dismiss();
+                    dialogFullScreen = null;
+                }catch (Exception ignored){
+
+                }
                 fullScreenContentCallback.onAdDismissedFullScreenContent();
             }
             return;
@@ -271,7 +276,12 @@ public class AppOpenManager implements Application.ActivityLifecycleCallbacks, L
                     new FullScreenContentCallback() {
                         @Override
                         public void onAdDismissedFullScreenContent() {
-                            dialogFullScreen.dismiss();
+                            try {
+                                dialogFullScreen.dismiss();
+                                dialogFullScreen = null;
+                            }catch (Exception ignored){
+
+                            }
                             // Set the reference to null so isAdAvailable() returns false.
                             appResumeAd = null;
                             if (fullScreenContentCallback != null) {
@@ -284,7 +294,13 @@ public class AppOpenManager implements Application.ActivityLifecycleCallbacks, L
 
                         @Override
                         public void onAdFailedToShowFullScreenContent(AdError adError) {
-                            dialogFullScreen.dismiss();
+                            try {
+                                dialogFullScreen.dismiss();
+                                dialogFullScreen = null;
+                            }catch (Exception ignored){
+
+                            }
+
                             if (fullScreenContentCallback != null) {
                                 fullScreenContentCallback.onAdFailedToShowFullScreenContent(adError);
                             }
@@ -405,7 +421,12 @@ public class AppOpenManager implements Application.ActivityLifecycleCallbacks, L
         dialogFullScreen.setCancelable(false);
         dialogFullScreen.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
         dialogFullScreen.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        dialogFullScreen.show();
+        try {
+            dialogFullScreen.show();
+        }catch (Exception ignored){
+
+        }
     }
+
 }
 
